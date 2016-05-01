6209733b393455b4522b7b69ba2c4d22c5ad2a53
Luiz Henrique Rapatao <rapatao@rapatao.com>
2016-05-01 16:12:03 -0300

    refactory in request handle registry and creation for exception handlers

---
+++ spark-spring-core/src/main/java/com/github/rascorp/spark/spring/exceptions/SparkSpringException.java
@@ -0,0 +1,8 @@
+package com.github.rascorp.spark.spring.exceptions;
+
+/**
+ * @author Luiz Henrique Rapatao - rapatao@rapatao.com
+ * @since 01/05/2016.
+ */
+public class SparkSpringException {
+}
---
+++ spark-spring-core/src/main/java/com/github/rascorp/spark/spring/registry/controller/SparkMethodRegistry.java
@@ -0,0 +1,8 @@
+package com.github.rascorp.spark.spring.registry.controller;
+
+/**
+ * @author Luiz Henrique Rapatao - rapatao@rapatao.com
+ * @since 01/05/2016.
+ */
+public class SparkMethodRegistry {
+}
